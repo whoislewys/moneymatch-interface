d084208f9eeb65ab9c7050aa74379d4c0ecfe79a
whoislewys <whoislewys@gmail.com>
2023-02-28 15:52:13 -0700

    network

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,26 +1,28 @@
-import { getDefaultWallets } from '@rainbow-me/rainbowkit'
-import { configureChains, createClient } from 'wagmi'
-import { localhost, polygonMumbai } from 'wagmi/chains'
-import { publicProvider } from 'wagmi/providers/public'
+import { getDefaultWallets } from '@rainbow-me/rainbowkit';
+import { configureChains, createClient } from 'wagmi';
+import { localhost, polygonMumbai } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
 
-console.log(import.meta.env?.MODE)
+console.log(import.meta.env?.MODE);
 const { chains, provider, webSocketProvider } = configureChains(
-  [...(import.meta.env?.MODE === 'development' ? [localhost] : []), polygonMumbai],
   [
-    publicProvider(),
+    ...(import.meta.env?.MODE === 'development'
+      ? [localhost]
+      : [polygonMumbai, localhost]),
   ],
-)
+  [publicProvider()]
+);
 
 const { connectors } = getDefaultWallets({
   appName: 'My wagmi + RainbowKit App',
   chains,
-})
+});
 
 export const client = createClient({
   autoConnect: true,
   connectors,
   provider,
   webSocketProvider,
-})
+});
 
-export { chains }
+export { chains };

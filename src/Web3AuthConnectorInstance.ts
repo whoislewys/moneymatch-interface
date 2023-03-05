255c38132b08fc3f3811a6ec06cf593ddb6e8c1c
whoislewys <whoislewys@gmail.com>
2023-03-04 18:44:46 -0700

    add base chainid + cyan web3auth mainnet to configs

--- src/Web3AuthConnectorInstance.ts
+++ src/Web3AuthConnectorInstance.ts
@@ -1,14 +1,14 @@
 // Web3Auth Libraries
-import { Web3AuthConnector } from "@web3auth/web3auth-wagmi-connector";
-import { Web3Auth } from "@web3auth/modal";
-import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
-import { CHAIN_NAMESPACES } from "@web3auth/base";
-import { Chain } from "wagmi";
+import { Web3AuthConnector } from '@web3auth/web3auth-wagmi-connector';
+import { Web3Auth } from '@web3auth/modal';
+import { OpenloginAdapter } from '@web3auth/openlogin-adapter';
+import { CHAIN_NAMESPACES } from '@web3auth/base';
+import { Chain } from 'wagmi';
 
 export default function Web3AuthConnectorInstance(chains: Chain[]) {
   // Create Web3Auth Instance
-  const name = "Money Match";
-  const iconUrl = "https://web3auth.io/docs/contents/logo-ethereum.png";
+  const name = 'Money Match';
+  const iconUrl = 'https://web3auth.io/docs/contents/logo-ethereum.png';
   const clientId = import.meta.env?.VITE_WEB3AUTH_CLIENT_ID;
   // const activeChainId = "0x13881"; // Mumbai
   const activeChainId = '0x14A33'; // base
@@ -23,12 +23,12 @@ export default function Web3AuthConnectorInstance(chains: Chain[]) {
   // Add openlogin adapter for customisations
   const openloginAdapterInstance = new OpenloginAdapter({
     adapterSettings: {
-      network: "testnet",
+      network: import.meta.env?.MODE === 'development' ? 'testnet' : 'cyan',
       whiteLabel: {
-        name: "Money Match",
-        logoLight: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
-        logoDark: "https://web3auth.io/images/w3a-D-Favicon-1.svg",
-        defaultLanguage: "en",
+        name: 'Money Match',
+        logoLight: 'https://web3auth.io/images/w3a-L-Favicon-1.svg',
+        logoDark: 'https://web3auth.io/images/w3a-D-Favicon-1.svg',
+        defaultLanguage: 'en',
         dark: true, // whether to enable dark mode. defaultValue: false
       },
     },
